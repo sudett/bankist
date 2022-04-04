02c31850d3d1cff5c74946f422fceed02b3058ef
sudett <sudett4978@gmail.com>
2022-04-04 14:12:42 +0430

    added features and operations sections, also implemented smooth scrolling and tabbed component functionalities

--- script.js
+++ script.js
@@ -7,8 +7,14 @@ const overlay = document.querySelector(".overlay");
 const modal = document.querySelector(".modal");
 const btnNav = document.querySelector(".btn--nav");
 const btnModal = document.querySelector(".btn--modal");
+const btnTxt = document.querySelector(".btn--txt");
+const sectionFeatures = document.getElementById("section-features");
+const navList = document.querySelector(".nav__list");
+const tabContainer = document.querySelector(".operations__tab-container");
+const operationsTabs = document.querySelectorAll(".operations__tab");
+const operationsContents = document.querySelectorAll(".operations__content");
 
-// Header hover functionality
+/////////// Header hover functionality
 navItems.forEach((navItem) => {
   navItem.addEventListener("mouseover", (e) => {
     navItems.forEach((navItem) => (navItem.style.opacity = 0.5));
@@ -22,15 +28,66 @@ navItems.forEach((navItem) => {
   });
 });
 
-// Modal functionality
+/////////// Modal functionality
+const closeModal = () => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+//open modal on clicking btnNav
 btnNav.addEventListener("click", () => {
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 });
 
+//close modal on cliking button close, overlay, button modal
 [btnModal, btnClose, overlay].forEach((item) =>
-  item.addEventListener("click", () => {
-    modal.classList.add("hidden");
-    overlay.classList.add("hidden");
-  })
+  item.addEventListener("click", closeModal)
 );
+
+//close modal on press scape key
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") closeModal();
+});
+
+/////////// Smooth scrolling
+btnTxt.addEventListener("click", (e) => {
+  //prevent link default functionality
+  e.preventDefault();
+
+  sectionFeatures.scrollIntoView({ behavior: "smooth" });
+});
+
+//page navigation
+navList.addEventListener("click", (e) => {
+  if (!e.target.classList.contains("nav__link")) return;
+
+  e.preventDefault();
+  const id = e.target.getAttribute("href");
+  document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+});
+
+/////////// Tabbed component
+tabContainer.addEventListener("click", (e) => {
+  const clickedBtn = e.target.closest(".operations__tab");
+
+  if (!clickedBtn) return;
+
+  // Remove active class from all tabs
+  operationsTabs.forEach((tab) =>
+    tab.classList.remove("operations__tab--active")
+  );
+
+  // Add active class to the target tab
+  clickedBtn.classList.add("operations__tab--active");
+
+  // Hide all contents
+  operationsContents.forEach((content) =>
+    content.classList.remove("operations__content--active")
+  );
+
+  // Display target content
+  document
+    .querySelector(`.operations__content--${clickedBtn.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
